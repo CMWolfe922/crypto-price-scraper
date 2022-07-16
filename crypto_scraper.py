a21e1192d0ed42292e74ebc2389a26a650f8dacc
C M Wolfe <cmwolfe.dev@gmail.com>
2022-07-16 02:49:55 -0500

    Imported the requirments from selenium and updated the requirments file

---
+++ crypto_scraper.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium import webdriver
